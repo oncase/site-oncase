e871bffa60e0632d1f7638e0002ab571ffd2044a
Rafael Fernandes <spnrafael@gmail.com>
2018-05-24 15:39:48 -0300

    Atualiza script de importação

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+cd oncase.com.br
+rvm use ruby-2.5.0@oncase
+git pull
+RAILS_ENV=production rails assets:precompile
+RAILS_ENV=production rails rails db:migrate
+touch 'tmp/restart.txt'
