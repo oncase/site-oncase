19ee03eef35b3b3e7191ec66ec259f9a108647e7
Rafael Fernandes <spnrafael@gmail.com>
2018-05-24 21:38:16 -0300

    Remove script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd oncase.com.br
-rvm use ruby-2.5.0@oncase
-git pull
-RAILS_ENV=production rails assets:precompile
-RAILS_ENV=production rails rails db:migrate
-touch 'tmp/restart.txt'
